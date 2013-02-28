76f1c347d941e5bbb65a18f37c214703a604dec9
Adesina <adesina.lalude@gmail.com>
2013-02-28 18:48:17 -0500

    Initial Commit

---
+++ CardMatch/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  CardMatch
+//
+//  Created by Akinbiyi Lalude on 2/28/13.
+//  Copyright (c) 2013 Akinbiyi Lalude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ CardMatch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CardMatch
+//
+//  Created by Akinbiyi Lalude on 2/28/13.
+//  Copyright (c) 2013 Akinbiyi Lalude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
